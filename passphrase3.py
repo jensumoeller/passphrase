93412ecec44a4529897fdf2d52dafc9cbe3e5cee
Jens U. Moeller <jensumoeller@me.com>
2023-05-21 14:35:00 +0200

    Neue Version 3 von passphrase.py Abfrage, ob weitere Phrase erstellt werden soll

---
+++ passphrase3.py
@@ -0,0 +1,38 @@
+# Generator für eine Passphrase variabler Länge auf Basis einer deutschen
+# DiceWare Liste, Selektion erfolgt über Random Funktion
+
+import pandas as pd
+import os
+import sys
+import random
+
+# Einlesen der DiceWare.txt Datei in eine Lise
+diceware = pd.read_csv("./diceware_de.csv", dtype={'zahl' : int, 'wort' : str})
+
+# Loop für Abfragewiederholung
+
+while True:
+
+    # Abfragen, wie viele Wörter die Passphrase haben soll
+    numberOfWords = int(input(" Anzahl der Wörter für die Passphrase: "))
+    
+    # Loop für Anzahl der Wörter
+    phrase = ""
+    for i in range(numberOfWords):
+    
+        # Wort per Zufallsfunktion aus der Spalte 'wort' der Tabelle diceware lesen
+        wort = random.choice(diceware.wort)
+        # Anhängen des wortes an die Phrase
+        phrase = phrase + str(wort) + " "
+
+    # Ausgabe des Satzes (der Passphrase)
+    print('\n', phrase, '\n')
+
+    # Abfrage, ob weitere Passphrase generiert werden soll
+    while True:
+        jaNein = str(input(" Neue Abfrage (j/n)?"))
+        if jaNein in ['j', 'J', 'n', 'N']:
+            break
+    if jaNein in ['n', 'N']:
+        break
+
